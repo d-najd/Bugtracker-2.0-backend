d4db2614dbbc719ff9d92627f4d63c6ff8a86f9a
d najd <dimitar.hristijan.najdovski@gmail.com>
2022-12-30 22:16:12 +0100

    swapping with maven instead of groovy

---
+++ user-project-service/src/main/java/io/dnajd/userprojectservice/UserProjectServiceApplication.java
@@ -0,0 +1,13 @@
+package io.dnajd.userprojectservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProjectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProjectServiceApplication.class, args);
+	}
+
+}
---
+++ user-project-service/src/test/java/io/dnajd/userprojectservice/UserProjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dnajd.userprojectservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProjectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
