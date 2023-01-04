3fde74300e2ca81ed58c794edd06b2b511f053f8
d najd <dimitar.hristijan.najdovski@gmail.com>
2023-01-04 20:12:33 +0100

    cleanup

--- project-table-service/src/main/kotlin/io/dnajd/projecttableservice/model/ProjectTableRepository.kt
+++
@@ -1,7 +0,0 @@
-package io.dnajd.projecttableservice.model
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ProjectTableRepository: JpaRepository<ProjectTable, Long> {
-
-}
