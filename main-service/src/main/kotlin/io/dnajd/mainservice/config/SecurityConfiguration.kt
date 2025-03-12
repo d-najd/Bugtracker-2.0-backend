48a852fe6a0ce95e23971e627603cbbee482759f
d-najd <dimitar.hristijan.najdovski@gmail.com>
2025-03-12 20:25:07 +0100

    feat!: something broke?

--- main-service/src/main/kotlin/io/dnajd/mainservice/config/SecurityConfiguration.kt
+++ main-service/src/main/kotlin/io/dnajd/mainservice/config/SecurityConfiguration.kt
@@ -1,36 +1,39 @@
 package io.dnajd.mainservice.config
 
+import io.dnajd.mainservice.infrastructure.JwtAudienceValidator
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
+import org.springframework.security.oauth2.core.OAuth2TokenValidator
+import org.springframework.security.oauth2.jwt.*
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
-// @EnableMethodSecurity
-public class SecurityConfiguration {
-    companion object {
-        public const val clientSecret = "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"
-        public const val clientId = "523144607813-ccib1llvilpg1e6httmo9a0d839bhh9h.apps.googleusercontent.com"
-        public const val testClientId = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjkxNGZiOWIwODcxODBiYzAzMDMyODQ1MDBjNWY1NDBjNmQ0ZjVlMmYiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI1MjMxNDQ2MDc4MTMtZm5uZzNwbWFpNDQ2anQwYWpjMDEyYWF2NmtlZG84dTQuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI1MjMxNDQ2MDc4MTMtY2NpYjFsbHZpbHBnMWU2aHR0bW85YTBkODM5YmhoOWguYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTUxMTAwNTQ4MzAzNjc5MDc0NjUiLCJlbWFpbCI6ImhmaGpkc2Jic2hzQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJuYW1lIjoiSGZoamQgU2Jic2hzIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FDZzhvY0l5ZnZLWVMyM0toeEcweU5Kell5dTRUZGl4Vm02S012OGprN2lRdjVIR1BKeXNvQT1zOTYtYyIsImdpdmVuX25hbWUiOiJIZmhqZCIsImZhbWlseV9uYW1lIjoiU2Jic2hzIiwiaWF0IjoxNzQxNzA5MDY0LCJleHAiOjE3NDE3MTI2NjR9.AtG7hjTVryMbhJiQV9LNsIEY8cAqf-CGbeYT2wiV9NH6ed7fxyC9hs4RXhrg-Awrs2pjVOUTFtgDBsFB_JzJlEU7fl7XKCFxxWg9woKHFxRlgFiO76t3YwJy8dv8jJpiwekJA4ISFn7oKCR8G-nxzlvHMynFk3dSWIP9SRora8gla74Ts9ikIRysi323j6-v4Iv8OUjiTGDaMgUXDwoaKaLjr4j5-bkbo4hck9R9acgJ649otlhJ1lPSlVZNsghfWIaT6T7NvXm_Hz19QtaiBhUzupyPz6_Bho8q2o7xe325uHtkRUDZBYiGW6Mp4xAcJVy2q9wfA-rM5yIt-zNjmw"
-    }
-
+class SecurityConfiguration(
+    @Value("\${spring.security.oauth2.resourceserver.jwt.issuer-uri}") private val issuerUrl: String,
+    @Value("\${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") private val jwkSetUrl: String,
+    @Value("\${spring.security.oauth2.resourceserver.jwt.audiences}") private val audiences: List<String>
+) {
     @Bean
-    @Throws(Exception::class)
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        return http.csrf { it.disable() }
-            .authorizeRequests()
-            .requestMatchers("/**")
-            // .antMatchers("/authenticate", "/register")
-            .permitAll()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .build()
+        return http.cors { cors -> cors.disable() }
+            .authorizeHttpRequests { auth ->
+                auth.anyRequest().authenticated()
+            }.oauth2ResourceServer { oauth2 -> oauth2.jwt { decoder() } }.build()
     }
 
+    fun decoder(): JwtDecoder {
+        val validators = mutableListOf<OAuth2TokenValidator<Jwt>>()
+        validators.add(JwtTimestampValidator())
+        validators.add(JwtIssuerValidator(issuerUrl))
+        // validators.add(JwtAudienceValidator(audiences))
+
+        val decoder = NimbusJwtDecoder.withJwkSetUri(jwkSetUrl)
+        decoder.jwsAlgorithms { DelegatingOAuth2TokenValidator(validators) }
+        return decoder.build()
+    }
 }
--- main-service/src/main/kotlin/io/dnajd/mainservice/controller/ProjectController.kt
+++ main-service/src/main/kotlin/io/dnajd/mainservice/controller/ProjectController.kt
@@ -1,13 +1,10 @@
 package io.dnajd.mainservice.controller
 
-import io.dnajd.mainservice.config.validateGoogleJwt
-import io.dnajd.mainservice.util.JwtTokenUtil
 import io.dnajd.mainservice.domain.project.ProjectDto
 import io.dnajd.mainservice.domain.project.ProjectDtoList
 import io.dnajd.mainservice.domain.project.ProjectList
 import io.dnajd.mainservice.infrastructure.Endpoints
 import io.dnajd.mainservice.service.project.ProjectService
-import io.jsonwebtoken.Jwts
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
@@ -15,30 +12,11 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping(Endpoints.PROJECT)
 class ProjectController(
     private val projectService: ProjectService,
-    private val jwtTokenUtil: JwtTokenUtil
 ) {
 
     @GetMapping("/testing/getAll")
     fun findAll(): ProjectList {
         return projectService.findAll()
-    }
-
-    @GetMapping("/testing/token/{token}")
-    fun isTokenValid(@PathVariable token: String): Boolean {
-        /*
-        if (!jwtTokenUtil.isTokenValid(token)) {
-            throw IllegalArgumentException()
-        }
-         */
-
-        /*
-        Jwts
-            .parserBuilder()
-            .requireId("523144607813-ccib1llvilpg1e6httmo9a0d839bhh9h.apps.googleusercontent.com")
-            .build()
-         */
-
-        return validateGoogleJwt(token, "523144607813-ccib1llvilpg1e6httmo9a0d839bhh9h.apps.googleusercontent.com")
     }
 
     @GetMapping("/user/{username}")
