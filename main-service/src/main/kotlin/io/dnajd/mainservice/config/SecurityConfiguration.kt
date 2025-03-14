684b16d487cce6e9826602a4bc8157a0a7024348
d-najd <dimitar.hristijan.najdovski@gmail.com>
2025-03-14 14:07:19 +0100

    fix: fixing bug with fetching child issues

--- main-service/src/main/kotlin/io/dnajd/mainservice/config/SecurityConfiguration.kt
+++ main-service/src/main/kotlin/io/dnajd/mainservice/config/SecurityConfiguration.kt
@@ -22,7 +22,9 @@ class SecurityConfiguration(
 ) {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        return http.cors { cors -> cors.disable() }
+        return http
+            .cors { cors -> cors.disable() }
+            .csrf { csrf -> csrf.disable() }
             .authorizeHttpRequests { auth ->
                 auth.anyRequest().permitAll()
             }.oauth2ResourceServer { oauth2 -> oauth2.jwt { decoder() } }.build()
