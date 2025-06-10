78c948235f2b2e20371bb91244b277491bedfe7c
d-najd <dimitar.hristijan.najdovski@gmail.com>
2025-06-10 18:50:11 +0200

    feat: added basic PermissionEvaluation (test of concept) for a method

--- src/main/kotlin/io/dnajd/mainservice/config/CustomPermissionEvaluator.kt
+++ src/main/kotlin/io/dnajd/mainservice/config/CustomPermissionEvaluator.kt
@@ -1,18 +1,33 @@
 package io.dnajd.mainservice.config
 
+import io.dnajd.mainservice.domain.project.Project
+import io.dnajd.mainservice.repository.ProjectAuthorityRepository
 import org.springframework.security.access.PermissionEvaluator
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Component
 import java.io.Serializable
 
 @Component
-class CustomPermissionEvaluator : PermissionEvaluator {
+class CustomPermissionEvaluator(
+    private val projectAuthorityRepository: ProjectAuthorityRepository
+) : PermissionEvaluator {
     override fun hasPermission(
         authentication: Authentication,
         targetDomainObject: Any,
         permission: Any
     ): Boolean {
-        TODO("Not yet implemented")
+        val userDetails = authentication.principal as UserDetails
+
+        when (targetDomainObject) {
+            is Project -> {
+                return hasProjectPermission(userDetails, targetDomainObject.id, permission)
+            }
+
+            else -> {
+                throw ClassNotFoundException(targetDomainObject::class.simpleName)
+            }
+        }
     }
 
     override fun hasPermission(
@@ -21,10 +36,21 @@ class CustomPermissionEvaluator : PermissionEvaluator {
         targetType: String,
         permission: Any
     ): Boolean {
-        TODO("Not yet implemented")
+        val userDetails = authentication.principal as UserDetails
+
+        when (targetType) {
+            "Project" -> {
+                return hasProjectPermission(userDetails, targetId as Long, permission)
+            }
+
+            else -> {
+                throw ClassNotFoundException(targetType)
+            }
+        }
     }
 
-    fun hasProjectPermission() {
-
+    fun hasProjectPermission(userDetails: UserDetails, projectId: Long, permission: Any): Boolean {
+        return projectAuthorityRepository.findByUsernameAndProjectId(userDetails.username, projectId)
+            .any { o -> o.authority == permission }
     }
 }
